24b88152389aef12c377b595f425a270616bd20e
dev-araujo <theriancirculo@protonmail.com>
2025-04-02 16:44:45 -0300

    chore: storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,16 @@
+import type { StorybookConfig } from '@storybook/angular';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/angular',
+    options: {},
+  },
+  staticDirs: ['../src/assets'],
+};
+export default config;
